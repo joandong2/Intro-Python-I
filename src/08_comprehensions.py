c9caa63d818b21835b2fc8d12b90d7327bc4e281
John Oblenda <joandong2@gmail.com>
2020-08-06 22:50:51 -0400

    10-15

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -14,6 +14,7 @@ y = []
 for x in range(1, 6):
     y.append(x)
 print(y)
+# y = [i for i in range(6)]
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
@@ -22,6 +23,7 @@ y = []
 for x in range(10):
     y.append(x**3)
 print(y)
+# y = [i**3 for i in range(10)]
 
 # Write a list comprehension to produce the uppercase version of all the
 # elements in array a. Hint: "foo".upper() is "FOO".
@@ -31,6 +33,7 @@ y = []
 for str in range(len(a)):
     y.append(a[str].upper())
 print(y)
+# y = [s.upper() for s in a]
 
 # Use a list comprehension to create a list containing only the _even_ elements
 # the user entered into list x.
@@ -43,3 +46,8 @@ for num in range(len(x)):
     if(int(x[num]) % 2) == 0:
         y.append(x[num])
 print(y)
+
+# y = [i for i in x if i % 2 == 0]
+# for i in x:
+#   if i % 2 == 0
+#       y.append(i)
--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+f = open("c:/Users/user/Desktop/LAMBDA/Computer Science/Intro-Python-I/src/foo.txt", "r")
+print(f.read())
+f.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +20,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+f = open("c:/Users/user/Desktop/LAMBDA/Computer Science/Intro-Python-I/src/bar.txt", "w")
+f.write("Do you bite your thumb at us, sir?\nI do bite my thumb, sir.\nDo you bite your thumb at us, sir?")
+f.close()
+
+f = open("c:/Users/user/Desktop/LAMBDA/Computer Science/Intro-Python-I/src/bar.txt", "r")
+print(f.read())
+f.close()
